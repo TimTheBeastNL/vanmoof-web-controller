fbbdaf81b10c503ac1be1514b36415bd81f335ea
TimTheBeastNL <118755239+TimTheBeastNL@users.noreply.github.com>
2023-08-18 16:01:30 +0200

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,8 +69,11 @@ const Home: NextPage = () => {
   return (
     <div>
       <main>
-        <h1 className='title'>Mooovy</h1>
+      <h1 className='title'>Custom Vanmoof horn</h1>
         <p>Change VanMoof S&X 3 speed limit</p>
+        <a style="buttonsounds" href="/mp4/imperial.mp4" download="imperial.mp4" >
+        imperial march
+        </a>
 
         {!browserCompatible || (!bikeInstance && !credentials) ?
           <>
