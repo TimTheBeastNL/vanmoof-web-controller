41890eaf9786cfd7f932a2b49934ebd69fc4f8ec
TimTheBeastNL <118755239+TimTheBeastNL@users.noreply.github.com>
2023-08-18 16:37:54 +0200

    removed button

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,9 +71,7 @@ const Home: NextPage = () => {
       <main>
        <h1 className='title'>Custom Vanmoof horn</h1>
         <p>Change VanMoof S&X 3 speed limit</p>
-        <a class="buttonsounds" href="/mp4/imperial.mp4" download="imperial.mp4" >
-        imperial march
-        </a>
+        
 
         {!browserCompatible || (!bikeInstance && !credentials) ?
           <>
