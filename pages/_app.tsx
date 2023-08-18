90ff5bb7247f58f151db17a3c5db0b6fd272b3a0
TimTheBeastNL <118755239+TimTheBeastNL@users.noreply.github.com>
2023-08-18 23:41:15 +0200

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,11 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
   return <div>
     <Head>
-      <title>Tim - Custom horns</title>
+      <title>Tim's Custom horns</title>
       <link rel="icon" type="image/png" href="/compressed_logos/logo_full_64.png" />
       <link rel="manifest" href="/app.webmanifest"></link>
-      <meta name="description" content="Change the speed limit of your vanmoof S3 and X3" />
-      <meta property="og:title" content="Change the speed limit of your vanmoof S3 and X3" />
+      <meta name="description" content="Add an custom horn to your S3 and X3." />
+      <meta property="og:title" content="Add an custom horn to your S3 and X3." />
       <meta property="og:image" content="https://mooovy.app/screenshot_dark.png" />
     </Head>
     <Component {...pageProps} />
