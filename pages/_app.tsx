ab62ef6ee02ca82afa54559dbaa2873435e98da8
TimTheBeastNL <118755239+TimTheBeastNL@users.noreply.github.com>
2023-08-18 23:43:14 +0200

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
   return <div>
     <Head>
-      <title>Tim's Custom horns</title>
+     <title>Tim&apos;s Custom horns</title>
       <link rel="icon" type="image/png" href="/compressed_logos/logo_full_64.png" />
       <link rel="manifest" href="/app.webmanifest"></link>
       <meta name="description" content="Add an custom horn to your S3 and X3." />
