f2e3a433fc8703854e69323b1baf0c7f7457b120
Tim Kunst <118755239+TimTheBeastNL@users.noreply.github.com>
2023-08-28 12:25:05 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps }: AppProps) {
   return <div>
     <Head>
-     <title>Tim&apos;s Custom horns</title>
+     <title>Custom horns</title>
       <link rel="icon" type="image/png" href="/compressed_logos/logo_full_64.png" />
       <link rel="manifest" href="/app.webmanifest"></link>
       <meta name="description" content="Add an custom horn to your S3 and X3." />
