5180ed66c0bd50dbd6088d7ac99ca304acd08462
TimTheBeastNL <118755239+TimTheBeastNL@users.noreply.github.com>
2023-10-22 11:26:31 +0200

    Update downloads.tsx

--- pages/downloads.tsx
+++ pages/downloads.tsx
@@ -9,7 +9,7 @@ const DownloadsPage: React.FC = () => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         />
-        <link rel="stylesheet" type="text/css" href="/css/downloads.css" />
+        <link rel="stylesheet" type="text/css" href="/styles/downloads.css" />
 
         <style>
           {`
