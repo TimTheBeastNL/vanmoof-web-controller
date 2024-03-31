db9ba2bf9f86843edb37a87cd82f2c81be0a3ac4
Tim Kunst <118755239+TimTheBeastNL@users.noreply.github.com>
2024-03-31 20:05:49 +0200

    Update bike.ts

--- lib/bike.ts
+++ lib/bike.ts
@@ -196,9 +196,9 @@ export class Bike {
     }
 
     async initiateBellSoundTransfer(buffer: ArrayBuffer): Promise<any> {
-        const fileHeader = new Uint8Array(0)
+        const fileHeader = new Uint8Array(000009185c4a8bdd40)
         
-        return await this.bluetoothWrite(FIRMWARE_METADATA, new Uint8Array(000009185c4a8bdd40))
+        return await this.bluetoothWrite(FIRMWARE_METADATA, new Uint8Array(fileHeader))
     }
 
     async sendBellSoundChunk(chunk: ArrayBuffer): Promise<any> {
